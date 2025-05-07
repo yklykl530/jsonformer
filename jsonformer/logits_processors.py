3cfad3ae8526368f6008259dfbe1a93096ada886
kailu <yklyklmail@gmail.com>
2025-05-07 14:11:42 +0800

    Update logits_processors.py

--- jsonformer/logits_processors.py
+++ jsonformer/logits_processors.py
@@ -1,4 +1,4 @@
-from transformers import PreTrainedTokenizer, LogitsProcessor, StoppingCriteria
+from transformers import PreTrainedTokenizer, LogitsProcessor, StoppingCriteria, PreTrainedModel
 import torch
 
 class StringStoppingCriteria(StoppingCriteria):
